fad20dc64ff888f284480210c2189cf3ae2e370c
Nicholas Manning <Nicholas.Manning@equinox.com>
2018-08-13 13:00:45 -0400

    Adding lesson01.

---
+++ lesson01/README.md
@@ -0,0 +1,10 @@
+# lesson01
+
+## Requirements
+
+1. Ensure Flutter is [setup on your system](https://flutter.io/get-started/)
+
+## Run
+
+1. Start your iOS simulator or Android emulator.
+2. Run `flutter run`
---
+++ lesson01/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MaterialApp(
+      title: "Beautiful Locations",
+      home: Scaffold(
+        appBar: AppBar(title: Text("Kyoto, Japan")),
+        body: null,
+      )));
+}
