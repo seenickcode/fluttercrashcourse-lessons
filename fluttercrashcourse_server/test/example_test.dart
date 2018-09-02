f62e429f67984c81d45fe0e66669c399d5a08e10
Nick Manning <nicholas.manning@gmail.com>
2018-09-02 19:44:39 -0400

    Removing server.

--- fluttercrashcourse_server/test/example_test.dart
+++
@@ -1,9 +0,0 @@
-import 'harness/app.dart';
-
-Future main() async {
-  final harness = Harness()..install();
-
-  test("GET /example returns 200 {'key': 'value'}", () async {
-    expectResponse(await harness.agent.get("/example"), 200, body: {"key": "value"});
-  });
-}
