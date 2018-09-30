ac80ff9f38cef36cabae211bbdfe4389923d675e
Nick Manning <nicholas.manning@gmail.com>
2018-09-30 19:34:29 -0400

    lesson11 finalized

--- recipe02-lists/lesson11/test/unit/location_test.dart
+++ recipe02-lists/lesson11/test/unit/location_test.dart
@@ -4,11 +4,12 @@ import '../../lib/models/location.dart';
 
 void main() {
   test('test location deserialization', () {
-    //  server response
     const locationJSON =
         '{ "name": "Arashiyama Bamboo Grove", "url": "https://cdn-images-1.medium.com/max/2000/1*vdJuSUKWl_SA9Lp-32ebnA.jpeg", "facts": [{ "title": "Summary", "text": "This bamboo grove is on the outskirts of Kyoto." }] }';
 
     final locationMap = json.decode(locationJSON) as Map<String, dynamic>;
+
+    expect("Arashiyama Bamboo Grove", equals(locationMap['name']));
 
     final location = Location.fromJson(locationMap);
 
