92af208c7b31503035c496415a58c3eba6375824
Nick Manning <nicholas.manning@gmail.com>
2018-11-14 17:10:51 -0500

    feat: adding banner_image

--- module01/README.md
+++ module01/README.md
@@ -12,10 +12,4 @@ Scaffold, Nav bar, Test Data, Print
 
 ## Steps
 
-1. set up vscode
-    a. install the VSCode 'Flutter' extension
-    b. add `"editor.formatOnSave": true,` to VSCode user settings
-1. clear out main.dart
-2. create app.dart
-3. create models/location.dart
-
+1. create components/banner_image.dart
--- module01/lib/location_list.dart
+++ module01/lib/location_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'components/banner_image.dart';
 
 class LocationList extends StatelessWidget {
   @override
@@ -9,3 +10,18 @@ class LocationList extends StatelessWidget {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'components/banner_image.dart';
+
+// class LocationList extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: BannerImage(
+//           url: 'http://fluttercrashcourse.com/assets/images/fuji@3x.jpg',
+//           height: 300.0),
+//     );
+//   }
+// }
