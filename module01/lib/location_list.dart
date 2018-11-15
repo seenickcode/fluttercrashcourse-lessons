b18f573b3b0dc524887c4af0036dbcb1e1295f92
Nick Manning <nicholas.manning@gmail.com>
2018-11-15 08:45:35 -0500

    minor tweak to module01

--- module01/lib/location_list.dart
+++ module01/lib/location_list.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 import 'components/banner_image.dart';
+import 'models/location.dart';
 
 class LocationList extends StatelessWidget {
+  final List<Location> locations = Location.fetchAll();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: BannerImage(
-          url: 'http://fluttercrashcourse.com/assets/images/fuji@3x.jpg',
-          height: 300.0),
+        appBar: AppBar(),
+        body: ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: listItemBuilder,
+        ));
+  }
+
+  Widget listItemBuilder(BuildContext context, int index) {
+    final location = this.locations[index];
+    return Container(
+      height: 400.0,
+      child: Column(
+        children: [
+          BannerImage(url: location.url, height: 300.0),
+          Container(
+              padding: EdgeInsets.all(20.0),
+              child: Text(location.name, style: TextStyle(fontSize: 20.0))),
+        ],
+      ),
     );
   }
 }
