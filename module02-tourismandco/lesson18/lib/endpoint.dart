707b31584ce142f833f60a571594cb58557e7e58
Nicholas Manning <nicholas.manning@gmail.com>
2019-11-08 11:47:17 -0500

    Updating apiHost from fluttercrashcourse.com to api.fluttercrashcourse.com

--- module02-tourismandco/lesson18/lib/endpoint.dart
+++ module02-tourismandco/lesson18/lib/endpoint.dart
@@ -4,7 +4,7 @@ class Endpoint {
   // NOTE: not realistic! we'll configure environment-specific variables in a soon to be
   // upcoming lesson
   static const apiScheme = 'https';
-  static const apiHost = 'fluttercrashcourse.com';
+  static const apiHost = 'api.fluttercrashcourse.com';
   static const prefix = '/api/v1';
 
   static Uri uri(String path, {Map<String, dynamic> queryParameters}) {
