f4a1cdc8f741085167e9b07d7b61bbceeb2abb69
Nick Manning <hello@manninglabs.com>
2021-11-22 14:13:55 -0500

    Disabling cal pager background to not muck up the stack trace crash when relogging in

--- module03-steadycalendar/lesson02/README.md
+++ module03-steadycalendar/lesson02/README.md
@@ -1,3 +1,7 @@
 # lesson02
 
+- Login with Google using flutter_supabase
+- Using splash.dart to handle auth events and navigation for auth state transitions
+- Log out feature
+
 https://docs.google.com/document/d/1ZoVqDU9kk0ZGdIddEiEPLfSoN9iFlT7Pok0zSagRr8A/edit#
---
+++ module03-steadycalendar/lesson02/lib/screens/cal_pager/background.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import '/config/constants.dart';
+
+class Background extends StatelessWidget {
+  final String? backgroundSlug;
+
+  const Background(this.backgroundSlug, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // FIXME if imageUrl value gets broken, app will crash b/c of how Flutter decides to handle
+    // broken image URLs. make more resilient.
+    return Container();
+    // return Expanded(
+    //     child: CachedNetworkImage(
+    //   fit: BoxFit.cover,
+    //   imageUrl:
+    //       "${baseBackgroundImageURL(env['SUPABASE_PROJECT_ID']!)}/$backgroundSlug",
+    //   placeholder: (context, url) => const CircularProgressIndicator(),
+    //   errorWidget: (context, url, error) => const Icon(Icons.error),
+    // ));
+  }
+}
