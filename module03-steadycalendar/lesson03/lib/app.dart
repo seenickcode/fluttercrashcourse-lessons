8d80fa9724586fbf3a7f943799e9dea61149f6f4
Nick Manning <hello@manninglabs.com>
2021-12-24 16:30:48 -0500

    Added integration tests to module 03 to lesson 03

--- module03-steadycalendar/lesson03/lib/app.dart
+++ module03-steadycalendar/lesson03/lib/app.dart
@@ -3,9 +3,18 @@ import '/screens/splash/splash.dart';
 import '/screens/cal_pager/cal_pager.dart';
 import '/screens/intro/intro.dart';
 import '/screens/login_options/login_options.dart';
+import 'package:flutter/material.dart' as flutter;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:provider/provider.dart';
+import '/repositories/cal_repo_interface.dart';
+import '/repositories/cal_repo.dart';
+import '/providers/session.dart';
 
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+  final State<Splash> splashState;
+
+  const App(this.splashState, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,4 +29,23 @@ class App extends StatelessWidget {
       },
     );
   }
+}
+
+/// Performs initialization steps and then runs our app.
+Future<void> runAppWithOptions(
+    {String envFileName = '.env',
+    CalRepoInterface calendarRepository = const CalendarRepository(),
+    required State<Splash> splashState}) async {
+  flutter.WidgetsFlutterBinding.ensureInitialized();
+
+  await load(fileName: envFileName);
+
+  await Supabase.initialize(
+      url: 'https://${env['SUPABASE_PROJECT_ID']!}.supabase.co',
+      anonKey: env['SUPABASE_ANON_KEY']!,
+      debug: false);
+
+  flutter.runApp(ChangeNotifierProvider(
+      create: (context) => SessionProvider(calendarRepository),
+      child: App(splashState)));
 }
--- module03-steadycalendar/lesson03/lib/main.dart
+++ module03-steadycalendar/lesson03/lib/main.dart
@@ -1,22 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:provider/provider.dart';
-import '/repositories/cal_repo.dart';
-import '/providers/session.dart';
-import 'app.dart';
+import '/app.dart';
+import '/screens/splash/splash.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await load(fileName: '.env');
-
-  await Supabase.initialize(
-      url: 'https://${env['SUPABASE_PROJECT_ID']!}.supabase.co',
-      anonKey: env['SUPABASE_ANON_KEY']!,
-      debug: false);
-
-  runApp(ChangeNotifierProvider(
-      create: (context) => SessionProvider(const CalendarRepository()),
-      child: const App()));
+  runAppWithOptions(envFileName: '.env', splashState: SplashState());
 }
